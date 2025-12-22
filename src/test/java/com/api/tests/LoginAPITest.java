e95f3c2d55432f00bb132d8a68ad7171d2697a08
Mukesh <mukesh.kumar@nestgroup.net>
2025-12-22 18:11:44 +0530

    base framework for api testing with login

---
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -0,0 +1,28 @@
+package com.api.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.api.base.AuthService;
+import com.api.models.LoginRequest;
+import com.api.models.response.LoginResponse;
+
+import io.restassured.response.Response;
+
+public class LoginAPITest {
+
+	@Test(description = "verify login api is working")
+	public void loginTest() {
+
+		LoginRequest loginRequest = new LoginRequest("themukesh", "12345678");
+		AuthService authService = new AuthService();
+		Response response = authService.login(loginRequest);
+
+		LoginResponse loginResponse = response.as(LoginResponse.class);
+
+		System.out.println(loginResponse.getToken());
+		Assert.assertTrue(loginResponse.getToken() !=null);
+
+	}
+
+}
